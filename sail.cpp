0b02152c0dd562bb601d1935ad1b570df482dc59
Christophe <Francis.Brown@jh.edu>
2020-04-04 22:05:11 -0400

    Made this a game.

--- sail.cpp
+++ sail.cpp
@@ -101,7 +101,7 @@ class Lando : public VxOLCPGE
 public:
 
 	RigidBody testObj;
-	Pricipia physicsEngine; 
+	Pricipia physicsEngine;
 
 	Enviroment env;
 	Circle test;
@@ -111,18 +111,20 @@ public:
 	float nominalScreenHeight;
 	Real shockAngle;
 	Line mouseLine;
-	
-	Camera cam; 
-	
+
+	Camera cam;
+
 	float frameMaxDt; //!< The maximum simulation time that can go by between frames. Will cause a slow down if 1/fElapsedTime less  than this
-	float timeMultiplier =1.0f; //!< Set to one to no effect. Less than one causes a slow down
+	float timeMultiplier = 1.0f; //!< Set to one to no effect. Less than one causes a slow down
 	float frameDelay = 0.0f; //!< Delay between game time updates. Useful for debugging.
-	
+
 private:
 	double previousFrameTime = -1.0; //!< Previous realTime that the frame was rendered. See float frameDelay.
 	double realTime = 0.0; //!< Real Time Clock;
 	double missionElapsedTime = 0.0; //!< Game time
 	bool simRunning = false; //!< Basically game pause.
+	bool gameOver = false;
+	bool postGame = false;
 	float throttleRate = 1.0f / 5.0f; // 1 over seconds from 0 to full throttle;
 
 public:
@@ -131,24 +133,32 @@ public:
 		Pencil::host = this;
 		ship.host = this;
 		sAppName = "Lando";
-		
+
 	}
 	//! Reset game to as if the executable just started
 	void ResetGameState() {
 		missionElapsedTime = 0;
+		gameOver = false;
 		ship = Ship();
+		
 		ship.body.pos = olc::vf2d(0.0f, 0.0f);
 		ship.body.vel = olc::vf2d(0.0f, 0.0f);
+		
+		
 		cam.anchorPos = cam.pos = ship.body.pos;
 		cam.anchorVel = cam.vel = ship.body.vel;
 		cameraPos = cam.pos;
-		// Testing
-		ship.body.rot    = 0.0f;
+		// Set to a decent starting position.
+		ship.body.rot = (float)M_PI/2;
+		ship.setSail((float)M_PI / 3);
+
 		ship.body.rotDot = 0.0f;
+		
 		ship.body.inertiaMoment = INFINITY;
 		//! Updates the positions of the bodies.
-		
+
 		physicsEngine.PropgateState(0.0f);
+		simRunning = true;
 	};
 public:
 
@@ -158,11 +168,11 @@ public:
 		// TODO improve performance at large areas.
 		// Proceduredurally generate stars.
 		// Divide the screen up 
-		constexpr float blockSize= 9.0f;
+		constexpr float blockSize = 9.0f;
 		vf2d TL = screenTopLeft();
-		float X = floor(TL.x/blockSize)*blockSize- blockSize;
+		float X = floor(TL.x / blockSize)*blockSize - blockSize;
 		for (int i = 0; X < TL.x + screenDims.x + blockSize; i++) {
-			float Y = ceil(TL.y / blockSize)*blockSize +blockSize;
+			float Y = ceil(TL.y / blockSize)*blockSize + blockSize;
 			for (int j = 0; Y > TL.y - screenDims.y - blockSize; ) {
 				uint32_t blockID = (uint16_t)(X / blockSize) << 16 | (uint16_t)(Y / blockSize);
 				nProcGen = blockID;
@@ -171,12 +181,12 @@ public:
 					//int brightness = rndInt(50,200);
 					float intensity = rndFloat(0.001f, 2.0f);
 					// modulate brightness based on scale.
-					int brightness = (int)fmin(255.0f,intensity * pow(getPixelsPerMeter(), 1.5f));
+					int brightness = (int)fmin(255.0f, intensity * pow(getPixelsPerMeter(), 1.5f));
 					float x = rndFloat(0.0f, blockSize);
 					float y = rndFloat(0.0f, blockSize);
-					olc::vf2d starPos( X + x,Y - y );
+					olc::vf2d starPos(X + x, Y - y);
 					Pixel star = Pixel(brightness, brightness, brightness, 255);
-					Pixel rendered = clampedPixel(backgroundColor,star);
+					Pixel rendered = clampedPixel(backgroundColor, star);
 					Draw(toScreen(starPos), rendered);
 				}
 				//FillCircle(toScreen({ X,Y }), 10,RED);
@@ -191,7 +201,7 @@ public:
 	{
 
 		//Foil testFoil;
-		
+
 		/*std::fstream test("wrapAngle.txt",std::fstream::binary | std::fstream::out);
 		for (float alpha = -2 * M_PI ; alpha < 2 * M_PI ; alpha += 0.05) {
 			std::cout << alpha << "    " << std::to_string(wrapAngle(alpha)) << std::endl;
@@ -199,42 +209,42 @@ public:
 		}
 		test.close();
 		std::cin.get();*/
-		
-		
+
+
 		// Maybe this is a good solution? You could do post processing. but it seems a little clunky.
-		
+
 		nominalScreenHeight = screenDims.y;
-		
-		
+
+
 		// Below is permanent stuff
 		physicsEngine.bodies.push_back(&ship.body);
 		physicsEngine.addEnviroment(&env);
-		
-		
-		frameMaxDt = 1/200.0f;
+
+
+		frameMaxDt = 1 / 200.0f;
 		timeMultiplier = 1.0f;
 		screenOffset.x = ScreenWidth() / 2;
 		screenOffset.y = ScreenHeight() / 2;
 
-		
+
 
 		frameDelay = 1.0f;
 		ResetGameState();
 		Pencil::headers = { "time","sailAOA","keelAOA","normalSailForce",
-			"axialSailForce","normalKeelForce","AxialKeelForce","velX","velY","axDot","NormDot"};
+			"axialSailForce","normalKeelForce","AxialKeelForce","velX","velY","axDot","NormDot" };
 		return true;
 	}
 public:
 	bool OnEveryFrame(float fElapsedTime) override
 	{
-		
+
 		realTime += fElapsedTime;
-		
+
 
 		//! This variable is used for all physics simulations. fElapsed time is used for user input.
-		float frameTimeStep = fElapsedTime* timeMultiplier;	// the timeMultiplier has an effect.
+		float frameTimeStep = fElapsedTime * timeMultiplier;	// the timeMultiplier has an effect.
 		// Cause an intential slow down of the game to enforce maximum game dt
-		
+
 		if (frameTimeStep > (frameMaxDt)) {
 			frameTimeStep = frameMaxDt;
 		}
@@ -242,20 +252,30 @@ public:
 		//frameTimeStep = 0.0005f;
 
 
+		// Determine whether the sim should continue this frame.
+		bool simThisFrame = simRunning;
+		// Allow space to pause simulation, and do one frame at a time
+		if (simRunning && GetKey(Key::SPACE).bPressed) {
+			simThisFrame = false;
+			simRunning = false;
+		}
+		else if (simRunning || GetKey(Key::SPACE).bPressed) {
+			simThisFrame = true;
+		}
 		
 		olc::vf2d mousePos(toWorld(olc::vi2d(GetMouseX(), GetMouseY())));
 		olc::vi2d mouseLoopBack(toScreen(mousePos));
 		DrawBackground();
-		
+
 		DrawDebugLine(std::to_string(missionElapsedTime));
-		
+
 		DrawDebugLine("pX:" + std::to_string(ship.body.pos.x) + " pY:" + std::to_string(ship.body.pos.y));
 		DrawDebugLine("vX:" + std::to_string(ship.body.vel.x) + " vY:" + std::to_string(ship.body.vel.y));
 		DrawDebugLine("aX:" + std::to_string(ship.body.acc.x) + " aY:" + std::to_string(ship.body.acc.y));
-		DrawDebugLine("Rudder:" + std::to_string(ship.rudderAngle*180/M_PI));
-		
+		DrawDebugLine("Rudder:" + std::to_string(ship.rudderAngle * 180 / M_PI));
+
 		// USER INPUT
-		
+
 		//if (GetKey(olc::Key::SHIFT).bHeld) throttleRateModifer = 10.0f;
 		//if (GetKey(olc::Key::CTRL).bHeld) throttleRateModifer = 0.05f;
 		//
@@ -263,29 +283,31 @@ public:
 		if (GetKey(Key::TAB).bPressed) simRunning = !simRunning;
 		if (GetKey(Key::ENTER).bPressed)  ship.body.setFixed(!ship.body.isFixed());
 		if (GetKey(Key::R).bPressed)  ResetGameState();
-		
-		//----- Ship Controls
-		
-		// Rudder
-		float rudderRate = 5.0f;
-		if (GetKey(olc::Key::A).bHeld) { ship.setRudder(ship.getRudder() - rudderRate * frameTimeStep);}
-		if (GetKey(olc::Key::D).bHeld) { ship.setRudder(ship.getRudder() + rudderRate * frameTimeStep);}
-		float turnRate = 2.0f;
-		if (GetKey(olc::Key::LEFT).bHeld) { ship.body.rot += turnRate * frameTimeStep; }
-		if (GetKey(olc::Key::RIGHT).bHeld) { ship.body.rot -= turnRate * frameTimeStep; }
-		// Sail
-		float sailRate = 1.0f;
-		if (GetKey(olc::Key::UP).bHeld) { ship.setSail(ship.getSailSlackAngle() + sailRate * frameTimeStep); }
-		if (GetKey(olc::Key::DOWN).bHeld) { ship.setSail(ship.getSailSlackAngle() - sailRate * frameTimeStep); }
 
-		// 
+		//----- Ship Controls
+
+		// Rudder
+		if (simThisFrame)
+		{
+			float rudderRate = 5.0f;
+			if (GetKey(olc::Key::A).bHeld) { ship.setRudder(ship.getRudder() - rudderRate * frameTimeStep); }
+			if (GetKey(olc::Key::D).bHeld) { ship.setRudder(ship.getRudder() + rudderRate * frameTimeStep); }
+			float turnRate = 2.0f;
+			if (GetKey(olc::Key::LEFT).bHeld) { ship.body.rot += turnRate * frameTimeStep; }
+			if (GetKey(olc::Key::RIGHT).bHeld) { ship.body.rot -= turnRate * frameTimeStep; }
+			// Sail
+			float sailRate = 1.0f;
+			if (GetKey(olc::Key::UP).bHeld) { ship.setSail(ship.getSailSlackAngle() + sailRate * frameTimeStep); }
+			if (GetKey(olc::Key::DOWN).bHeld) { ship.setSail(ship.getSailSlackAngle() - sailRate * frameTimeStep); }
+		}
+				// 
 
 		//----- Camera Controls
-		constexpr float camMoveRate = 1000.0f; // m/s
-		if (GetKey(olc::Key::UP).bHeld) {    cameraPos.y += camMoveRate/getPixelsPerMeter() * fElapsedTime; }
-		if (GetKey(olc::Key::DOWN).bHeld) {  cameraPos.y -= camMoveRate/getPixelsPerMeter() * fElapsedTime; }
-		if (GetKey(olc::Key::RIGHT).bHeld) { cameraPos.x += camMoveRate/getPixelsPerMeter() * fElapsedTime; }
-		if (GetKey(olc::Key::LEFT).bHeld) {  cameraPos.x -= camMoveRate/getPixelsPerMeter() * fElapsedTime; }
+		constexpr float camMoveRate = 1000.0f; 
+		if (GetKey(olc::Key::W).bHeld) {    cameraPos.y += camMoveRate/getPixelsPerMeter() * fElapsedTime; }
+		if (GetKey(olc::Key::S).bHeld) {  cameraPos.y -= camMoveRate/getPixelsPerMeter() * fElapsedTime; }
+		if (GetKey(olc::Key::D).bHeld) { cameraPos.x += camMoveRate/getPixelsPerMeter() * fElapsedTime; }
+		if (GetKey(olc::Key::A).bHeld) {  cameraPos.x -= camMoveRate/getPixelsPerMeter() * fElapsedTime; }
 		//Zoom 													
 		float zoomRate = 2.0f;
 		if (GetKey(olc::Key::K1).bHeld) {
@@ -297,16 +319,6 @@ public:
 			setScreenHeightMeters(nominalScreenHeight);
 		}
 
-
-		// Determine whether the sim should continue this frame.
-		bool simThisFrame = simRunning;
-		// Allow space to pause simulation, and do one frame at a time
-		if (simRunning && GetKey(Key::SPACE).bPressed) {
-			simThisFrame = false;
-			simRunning = false;
-		} else if (simRunning || GetKey(Key::SPACE).bPressed) {
-			simThisFrame = true;
-		}
 
 		if (simThisFrame) {
 			const float dt = frameTimeStep;
@@ -326,10 +338,7 @@ public:
 			cameraPos = cam.pos;
 			ship.applyEnviromentForces(wind(ship.body.pos));
 			ship.updateState(dt);
-
 			physicsEngine.PropgateState(dt);
-;
-
 		}
 
 		if (GetMouse(1).bPressed) {
@@ -354,10 +363,30 @@ public:
 		DrawDebugLine("Sail     Angle:" + std::to_string(ship.sailAngleFromCenterline * 180 / M_PI));
 		DrawDebugLine("Heading       :" + std::to_string(ship.getHeading() * 180 / M_PI));
 		DrawDebugLine("Speed: " + std::to_string(ship.body.vel.mag()));
-		if (ship.body.pos.x < 10.0f) {
+		if (ship.body.pos.x < 10.0f && !postGame) {
 			DrawString(olc::vi2d(5, 460), "Use Arrow Keys to move. Race to upwind!");
 			DrawString(olc::vi2d(5, 470), "LEFT/RIGHT: Rudder");
 			DrawString(olc::vi2d(5, 480), "UP/DOWN Let sail out/pull in sail");
+		}
+
+		
+
+		if (gameOver && simRunning) {
+			postGame = true;
+		}
+
+		float winThreshold = 100.0f;
+		if ( (ship.body.pos.x >= winThreshold && simThisFrame || gameOver) && !postGame) {
+			simRunning = false;
+			gameOver = true;
+			
+			olc::Pixel msgColor = olc::GREY;
+			constexpr int H = 100;
+			DrawString(olc::vi2d(100, H+0),  "           Congratulations!",msgColor);
+			char S[100];  sprintf_s(S, 100,  "You made it %3.1f m upwind in %2.3f seconds.", winThreshold, missionElapsedTime);
+			DrawString(olc::vi2d(100, H+10), std::string(S),msgColor);
+			DrawString(olc::vi2d(100, H+20), "         Press R to restart.", msgColor);
+			DrawString(olc::vi2d(100, H+30), "      Else press TAB to unpause.", msgColor);
 		}
 		if (isnan(ship.body.pos.x) && simRunning) {
 			simRunning = false;
